06e3b43a1d82b66c769dd99639e2819631561ae4
Ayeromara <isaiahayeromara@gmail.com>
2025-03-05 12:25:26 +0100

    first commit

---
+++ windows/QuestED/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::QuestED::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::QuestED::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
